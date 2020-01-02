a872705ee25ce2e486890f2586eece06d450b084
Eli Reiners <goelyukim@gmail.com>
2020-01-02 12:38:41 -0500

    Set up Accordion app

---
+++ src/state-drill/Accordion.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+export default class Accordion extends Component {
+    state = {
+        current: 0
+      };
+    renderContent(index) {
+        return <p>{this.state.content}</p>
+    }
+    render() {
+
+        const sections = this.props.sections.map((section, index) =>
+            <li key={index}>
+                <button>
+                    {section.title}
+                    {(this.state.current === index) && this.renderContent(index)}
+                </button>
+            </li>);
+
+        return (
+            <ul>
+                {sections}
+            </ul>
+        )
+    }
+}
